ee813413c7c2265a2e678b6281884df572c7dffa
CarlosWarrior <carlos.alejandro.97@gmail.com>
2022-08-15 12:15:10 -0500

    csp

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ require('dotenv').config()
 const {readFileSync} = require('fs')
 const express = require('express')
 const app = express()
-app.use(require('helmet')())
+app.use(require('helmet')({
+  contentSecurityPolicy:{
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'blob:',],
+  
+      fontSrc: ["'self'", 'https:', 'data:', "'unsafe-inline'"],
+  
+      scriptSrc: ["'self'",'https:', "'unsafe-inline'"],
+  
+      scriptSrcElem: ["'self'",'https:','https:', "'unsafe-inline'"],
+  
+      styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+  
+      connectSrc: ["'self'", 'data','https:']
+    },
+  }
+}))
 app.use(require('cors')({origin:'*'}))
 app.use(express.json())
 app.use(express.urlencoded( {extended: true} ))
--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const path = require('path')
+const fs = require('fs')
 const {readFileSync} = require('fs')
 const {Router, static} = require('express')
 const logRequest = require('./middleware/log')
-const src = './storage/build'
+const src = './storage/build/'
 module.exports = Router()
 	.use(async(req,res,next) => next(res.locals=null))
 	.use(logRequest)
@@ -10,5 +11,18 @@ module.exports = Router()
 		const ssl = readFileSync("./public_access_files/ssl").toString()
 		res.send(ssl)
 	})
-	.get('/', (req,res) => res.sendFile('index.html', {root:src}) )
-	.use(static(src))
+	.get('/*', (req, res)=>{
+		if(req.originalUrl != "/" && req.originalUrl.indexOf(".") >= 0){
+			const root = path.join(src, req.originalUrl)
+			console.log(root, fs.existsSync(root))
+			if(fs.existsSync(root))
+				res.sendFile(root, {root:"."})
+			else
+				res.status(404).send()
+		}
+		else res.sendFile("index.html", {root:src})
+	})
+	/*
+	.use('/', static(src))
+	.use('/static/', static(src+'static/'))
+	*/
