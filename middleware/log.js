5cb33f7f41f2b88f87522535915f282a20ce0909
Carlos Alejandro Guerrero Martínez <carlos.alejandro.97@gmail.com>
2024-02-23 18:17:38 -0600

    added timestamp to log

--- middleware/log.js
+++ middleware/log.js
@@ -1,5 +1,5 @@
 function Log(req, res, next) {
-	console.log(`${req.ip} ${req.ips} ->\t ${req.method}@${req.get('host')} ${req.originalUrl}`)
+	console.log(`${req.ip} ${req.ips} ->\t ${req.method}@${req.get('host')} ${req.originalUrl}\t\tat ${(new Date()).toISOString()}`)
 	return next()
 }
 module.exports = Log
